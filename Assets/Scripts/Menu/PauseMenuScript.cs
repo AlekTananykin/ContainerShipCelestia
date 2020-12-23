c36a5b6e16ad5d6075f6a56c64dbaf514d8a897c
AlekTananykin <tananykin@mail.ru>
2020-12-23 22:43:08 +0300

    Standard menu

---
+++ Assets/Scripts/Menu/PauseMenuScript.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    bool _isPaused = false;
+
+    [SerializeField] private GameObject _pauseMenu;
+
+    private void Awake()
+    {
+        _pauseMenu.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+    }
+
+    void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Escape))
+            return;
+
+        if (_isPaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Pause()
+    {
+        _pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        _isPaused = true;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Resume()
+    {
+        _pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        _isPaused = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quite()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+    }
+}
